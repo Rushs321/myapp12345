6e6df69e159027306960fd872a346f83cd32d8e3
Brosko123 <ytur123@tutamail.com>
2024-10-30 16:20:10 +0530

    Create params.js

---
+++ src/params.js
@@ -0,0 +1,14 @@
+"use strict";
+const DEFAULT_QUALITY = 40
+
+export default function params(req, res, next) {
+  let url = req.query.url;
+  if (!url) return res.send('bandwidth-hero-proxy');
+
+  req.params.url = decodeURIComponent(url);
+  req.params.webp = !req.query.jpeg
+  req.params.grayscale = req.query.bw != 0
+  req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+
+  next()
+}
