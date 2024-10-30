8cfd7cdb6e1429fd9bed9b1ea739285ba4a6171a
Brosko123 <ytur123@tutamail.com>
2024-10-30 16:19:39 +0530

    Create copyHeaders.js

---
+++ src/copyHeaders.js
@@ -0,0 +1,10 @@
+"use strict";
+export default function copyHeaders(source, target) {
+  for (const [key, value] of Object.entries(source.headers)) {
+    try {
+      target.setHeader(key, value);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
