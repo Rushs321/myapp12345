5a5e6fbf5639ce558cd28df9b50737b16970fc4f
Brosko123 <ytur123@tutamail.com>
2024-10-30 16:13:27 +0530

    Create compress.js

---
+++ src/compress.js
@@ -0,0 +1,42 @@
+/*
+ * compress.js
+ * A module that compresses an image.
+ * compress(httpRequest, httpResponse, ReadableStream);
+ */
+import sharp from 'sharp';
+import redirect from './redirect.js';
+
+const sharpStream = () => sharp({ unlimited: true });
+
+export default function compress(req, res, input) {
+  const format = 'jpeg';
+
+  /*
+   * input.pipe => sharp (The compressor) => Send to httpResponse
+   * The following headers:
+   * |  Header Name  |            Description            |           Value            |
+   * |---------------|-----------------------------------|----------------------------|
+   * |x-original-size|Original photo size                |OriginSize                  |
+   * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
+   */
+  input.pipe(
+    sharpStream()
+      .grayscale(req.params.grayscale)
+      .toFormat(format, {
+        quality: req.params.quality
+      })
+      .toBuffer((err, output, info) => _sendResponse(err, output, info, format, req, res))
+  );
+}
+
+function _sendResponse(err, output, info, format, req, res) {
+  if (err || !info) return redirect(req, res);
+
+  res.setHeader('content-type', 'image/' + format);
+  res.setHeader('content-length', info.size);
+  res.setHeader('x-original-size', req.params.originSize);
+  res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+  res.status(200);
+  res.write(output);
+  res.end();
+}
