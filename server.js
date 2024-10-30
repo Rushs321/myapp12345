f988418bb29367c20d70fb1c8e732e43ed6d0175
Brosko123 <ytur123@tutamail.com>
2024-10-30 16:23:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-const app = require('./index');
-const server = require('http').createServer(app);
+import app from './index.js'; // Import the app from index.js
+import http from 'http'; // Import the http module
+
+const server = http.createServer(app);
 server.listen(process.env.PORT || 8080);
