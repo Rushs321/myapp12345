19fb8ddaac9f7776cbfdd2c0e0312a25ba7b9572
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-31 23:06:00 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -42,7 +42,8 @@ export default async function proxy(req, res) {
       cookieJar,
       timeout: {
         response: 6600 // ms
-      }
+      },
+      decompress: true
  //   throwHttpErrors: false, // We handle errors based on status code
  //   retry: { limit: 2 }, // Optionally, define retry limits (if needed)
 //    timeout: { request: 10000 }
