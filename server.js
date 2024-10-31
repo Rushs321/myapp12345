21ecf353816f92e44606b80dd3291c0e2fe70f53
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-31 22:21:43 +0530

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-import app from './index.js'; // Import the app from index.js
-import http from 'http'; // Import the http module
-
-const server = http.createServer(app);
-server.listen(process.env.PORT || 8080);
