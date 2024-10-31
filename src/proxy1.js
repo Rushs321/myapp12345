b3677b1123f32ecbd15566f2875b8e9602aceb0f
Brosko123 <ytur123@tutamail.com>
2024-10-31 11:31:52 +0530

    Update proxy1.js

--- src/proxy1.js
+++ src/proxy1.js
@@ -34,7 +34,8 @@ export default async function proxy(req, res) {
       throwHttpErrors: false, // We handle errors based on status code
       retry: { limit: 2 }, // Optionally, define retry limits (if needed)
       timeout: { request: 10000 },
-      http2: true
+      http2: true,
+      decompress: true
     };
 
     // Using got.stream to initiate the request and stream data
